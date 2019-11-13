d8573b572838c6ab09fc39a49894e621d4cc7b83
Bartosz <44466149+Cwiekson@users.noreply.github.com>
2019-11-13 09:34:36 +0100

    Add files via upload

---
+++ cw5/cw5/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw5
+{
+    class Car:Vehicle,IRideable
+    { 
+        public void Ride()
+    {
+        Console.WriteLine("Jadę samochodem");
+    }
+
+}
+    
+}
---
+++ cw5/cw5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Bicycle bicycle1 = new Bicycle();
+            car1.Ride();
+            bicycle1.Ride();
+            
+            Osoba osoba1 = new Osoba();
+            osoba1.Graj();
+            IGitarzysta franek = new Osoba();
+            ISkrzypek stasiu = new Osoba();
+            franek.Graj();
+            stasiu.Graj();
+            Console.ReadKey();
+        }
+    }
+}
